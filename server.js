48d1aba1fb63c301d10463fb9876fa8015ff925f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-10 12:56:58 +0300

    foundations

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const socket = require('socket.io');
+const { isObject } = require('util');
 
 const app = express();
-const server = app.listen(3000)
+const server = http.createServer(app);
 
-app.use(express.static('public'))
+//set static folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',socket=>{
+    console.log('New WS Connection...')
+});
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
