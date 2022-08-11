1b152806532d2cfaaaf979a32f09b5af7854e9fb
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-11 10:58:22 +0300

    configuration & messages

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socket = require('socket.io');
+const socketio = require('socket.io');
 const { isObject } = require('util');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder 
 app.use(express.static(path.join(__dirname,'public')));
 
 io.on('connection',socket=>{
-    console.log('New WS Connection...')
+    console.log('New WS Connection...');
+    socket.emit('message','Welcome to ChatApp:');
+    socket.broadcast.emit('message','A user has joined the chat');
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat')
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
